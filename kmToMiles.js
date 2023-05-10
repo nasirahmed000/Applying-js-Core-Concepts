fbc955dea5a5be0956a88d8a1d7b9b29a005f64b
Nasir Ahmed <ahsannasirahmed@gmail.com>
2023-05-10 22:34:34 +0600

    km

---
+++ kmToMiles.js
@@ -0,0 +1,9 @@
+function convertKmToMiles(kilometers) {
+    var miles = kilometers * 0.6213712;
+    return miles;
+  }
+  
+  var kilometers = 10; // Replace with your desired value in kilometers
+  var miles = convertKmToMiles(kilometers);
+  console.log(miles);
+  
--- sumOfArray.js
+++ sumOfArray.js
@@ -1,3 +1,4 @@
+
 function sumArray(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
