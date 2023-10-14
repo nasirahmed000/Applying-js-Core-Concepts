f5f83c47028b3f42feca8e9be1e9bf47dda602e4
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2023-10-14 23:10:55 +0600

    Update isLeapYear.js

--- isLeapYear.js
+++ isLeapYear.js
@@ -20,6 +20,26 @@ console.log("my year is leap year",herYearIs);
 
 
 
+function isLeapYear(year){
+    const remainder = year % 4;
+   
+    if (remainder === 0 ){
+        return true;
+    }
+    
+    else{
+     return  false;
+    }
+}
+
+const myYearIs= isLeapYear(1933);
+console.log("my year is leap year",myYearIs);
+const herYearIs= isLeapYear(1960);
+console.log("my year is leap year",herYearIs);
+
+
+
+
 
 
 
